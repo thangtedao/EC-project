b026becf59021da8fefef169371db94d002712bb
NhieuThinh <nguyennhieuthinh@gmail.com>
2024-04-25 14:40:32 +0700

    done comment-blog

---
+++ client/src/components/commentBlog/AllRepComment.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { WechatOutlined } from '@ant-design/icons';
+import { getFirstCharacterUser } from '../../utils/blog';
+
+function AllRepComment(props) {
+    const {allrepcomment, showRepComment, id} = props;
+    // console.log(allrepcomment)
+    
+    return (
+      <div className="all-comment-rep-list">
+        <div className="arrow-up"></div>
+        {allrepcomment.map((repComment) => (
+          <div className="all-comment-rep-list-item" key={repComment._id}>
+            <div className="all-comment-info">
+              {repComment.role=='admin' ? (
+                <div className="all-comment-info-name admin">
+                  <img src="https://cellphones.com.vn/skin/frontend/default/cpsdesktop/images/media/logo.png"></img>
+                </div>
+              ) : (
+                <div className="all-comment-info-name">
+                  {getFirstCharacterUser(repComment.author)}
+                  {'T'}
+                </div>
+              )}
+              {
+                repComment.role=='admin' ? (<strong>{repComment.author} <span>QTV</span></strong>): (<strong>{repComment.author}</strong>)
+              }
+              
+            </div>
+
+            <div className="all-comment-content">{repComment.content}</div>
+
+            <div className="all-comment-more">
+              <a
+                className="all-comment-more-chat"
+                onClick={() => showRepComment(id)}
+              >
+                <WechatOutlined style={{ color: "#e11b1e" }} /> <p> Trả lời</p>
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+}
+
+export default AllRepComment;
--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,55 +1,59 @@
-// import React from 'react'
-
-import { useParams } from "react-router-dom"
-import Wrapper from "../assets/wrappers/Blog"
-import { useEffect, useState } from "react"
-import customFetch from "../utils/customFetch"
-import axios from "axios"
-import moment from 'moment'
-import { HelmetProvider } from "react-helmet-async"
+import { useEffect, useState } from "react";
+import { HelmetProvider } from "react-helmet-async";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import moment from "moment";
+import Wrapper from "../assets/wrappers/Blog";
+import CommentBlog from "../components/commentBlog/CommentBlog";
+import customFetch from "../utils/customFetch";
 
 const Blog = () => {
-    const {id} = useParams()
-    const [blog, setBlog] = useState([])
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(`http://localhost:3001/api/blog/${id}`);
-                const data = response.data.blog;
-                // console.log(response.data.blog)
-                setBlog(data);
-          } catch (error) {
-            console.error("Error fetching data blog:", error);
-          }
-        };
-    
-        if (id) {
-          fetchData();
-          console.log(blog)
-        }
-    }, []);
+  const { id } = useParams();
+  const [blog, setBlog] = useState([]);
+  const [isLoading, setIsLoading] = useState(true); // Thêm biến trạng thái isLoading
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/api/blog/${id}`);
+        const data = response.data.blog;
+        setBlog(data);
+        setIsLoading(false); // Dữ liệu đã được fetch xong
+      } catch (error) {
+        console.error("Error fetching data blog:", error);
+      }
+    };
+
+    if (id) {
+      fetchData();
+    }
+  }, [id]);
+
   return (
     <HelmetProvider>
-        <Wrapper>
-            <div id='blog'>
-                <div className="blog">
-                    <div className="blog-header">
-                        <div className="blog-image">
-                            <img src={blog.imageTitle} alt='' />
-                        </div>
-                        <div className="blog-header-right">
-                            <div className="blog-title">{blog.title}</div>
-                            <div className="blog-lastupdated"> Last updated: {moment(blog.updatedAt).format('HH:mm, DD/MM/YYYY')}</div>
-                        </div>
-                    </div>
-                    <div className="blog-content">
-                            <div dangerouslySetInnerHTML={{ __html:blog.content}} />
-                    </div>
+      <Wrapper>
+        <div id="blog">
+          <div className="blog">
+            <div className="blog-header">
+              <div className="blog-image">
+                <img src={blog.imageTitle} alt="" />
+              </div>
+              <div className="blog-header-right">
+                <div className="blog-title">{blog.title}</div>
+                <div className="blog-lastupdated">
+                  Last updated: {moment(blog.updatedAt).format("HH:mm, DD/MM/YYYY")}
                 </div>
+              </div>
             </div>
-        </Wrapper>
+            <div className="blog-content">
+              <div dangerouslySetInnerHTML={{ __html: blog.content }} />
+            </div>
+          </div>
+        </div>
+        {!isLoading && <CommentBlog blog={blog} />} {/* Hiển thị CommentBlog khi isLoading là false */}
+      </Wrapper>
     </HelmetProvider>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
